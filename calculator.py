a036e2e4350b4b483f429febfc94f6acfdb8efd5
Bryan Ortega <fbryanortega@gmail.com>
2023-05-17 04:59:07 -0500

    completed initial REPL for calculator.py

--- calculator.py
+++ calculator.py
@@ -5,3 +5,31 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+    input_string = input("Enter an operation to complete: ")
+    token = input_string.split(" ")
+
+    operation = token[0]
+    num1 = int(token[1])
+    num2 = int(token[2])
+
+    if 'q' in token:
+        print("You will now quit the calculator.")
+        break
+    else:
+        if operation == '+':
+            print(add(num1, num2))
+        if operation == '-':
+            print(subtract(num1, num2))
+        if operation == '*':
+            print(multiply(num1, num2))
+        if operation == '/':
+            print(divide(num1, num2))
+        if operation == 'square':
+            print(square(num1))
+        if operation == 'cube':
+            print(cube(num1))
+        if operation == 'pow':
+            print(power(num1, num2))
+        if operation == 'mod':
+            print(mod(num1, num2))
