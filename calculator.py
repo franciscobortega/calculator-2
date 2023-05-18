e89eb5def73eb8e32c4984129b86cde4f5648a9f
Bryan Ortega <fbryanortega@gmail.com>
2023-05-17 21:27:27 -0500

    Added input validation

--- calculator.py
+++ calculator.py
@@ -7,29 +7,42 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 # Replace this with your code
 while True:
     input_string = input("Enter an operation to complete: ")
-    token = input_string.split(" ")
+    tokens = input_string.split(" ")
 
-    operation = token[0]
-    num1 = int(token[1])
-    num2 = int(token[2])
-
-    if 'q' in token:
-        print("You will now quit the calculator.")
+    if 'q' in tokens:
+        print("\tYou will now quit the calculator.")
         break
+
+    if len(tokens) < 2:
+        print("\tMissing a number or operation!")
+        continue
+
+    operation = tokens[0]
+    num1 = tokens[1]
+
+    if len(tokens) < 3:
+        num2 = 0
     else:
-        if operation == '+':
-            print(add(num1, num2))
-        if operation == '-':
-            print(subtract(num1, num2))
-        if operation == '*':
-            print(multiply(num1, num2))
-        if operation == '/':
-            print(divide(num1, num2))
-        if operation == 'square':
-            print(square(num1))
-        if operation == 'cube':
-            print(cube(num1))
-        if operation == 'pow':
-            print(power(num1, num2))
-        if operation == 'mod':
-            print(mod(num1, num2))
+        num2 = tokens[2]
+
+    if not num1.isdigit() or not num2.isdigit():
+        print("\tPlease provide numbers to run a calculation!")
+        continue
+    elif operation == '+':
+        print(add(num1, num2))
+    elif operation == '-':
+        print(subtract(num1, num2))
+    elif operation == '*':
+        print(multiply(num1, num2))
+    elif operation == '/':
+        print(divide(num1, num2))
+    elif operation == 'square':
+        print(square(num1))
+    elif operation == 'cube':
+        print(cube(num1))
+    elif operation == 'pow':
+        print(power(num1, num2))
+    elif operation == 'mod':
+        print(mod(num1, num2))
+    else:
+        print("\tcommand not recognized")
