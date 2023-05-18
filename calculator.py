a5d6be7f2e5d024954e26a7bbbbe01d48e17b215
Bryan Ortega <fbryanortega@gmail.com>
2023-05-17 21:54:53 -0500

    Made calculator recursive with dictionary for operations

--- calculator.py
+++ calculator.py
@@ -3,16 +3,28 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+operations = {
+  '+': add,
+  '-': subtract,
+  '*': multiply,
+  '/': divide,
+  'square': square,
+  'cube': cube,
+  'power': power,
+  'mod': mod
+}
 
 # Replace this with your code
 while True:
     input_string = input("Enter an operation to complete: ")
     tokens = input_string.split(" ")
 
+    # exit program
     if 'q' in tokens:
         print("\tYou will now quit the calculator.")
         break
 
+    # input validation for missing operator/operand
     if len(tokens) < 2:
         print("\tMissing a number or operation!")
         continue
@@ -21,28 +33,17 @@ while True:
     num1 = tokens[1]
 
     if len(tokens) < 3:
-        num2 = 0
+        num2 = "0"
     else:
         num2 = tokens[2]
 
     if not num1.isdigit() or not num2.isdigit():
         print("\tPlease provide numbers to run a calculation!")
         continue
-    elif operation == '+':
-        print(add(num1, num2))
-    elif operation == '-':
-        print(subtract(num1, num2))
-    elif operation == '*':
-        print(multiply(num1, num2))
-    elif operation == '/':
-        print(divide(num1, num2))
-    elif operation == 'square':
-        print(square(num1))
-    elif operation == 'cube':
-        print(cube(num1))
-    elif operation == 'pow':
-        print(power(num1, num2))
-    elif operation == 'mod':
-        print(mod(num1, num2))
+
+    if operation in operations:
+        calculation_function = operations[operation]
+        answer = calculation_function(float(num1), float(num2))
+        print(answer)
     else:
         print("\tcommand not recognized")
